7501e04741e55cd8b934fe8c642774760b7c304b
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2023-02-28 04:23:59 +0800

    Update update.py

--- bot/update.py
+++ bot/update.py
@@ -1,6 +1,6 @@
-version = 'v1.5.1'
+version = 'v1.4.0'
 botlog = """
-2023.2.19 (v1.5.1)
-    * 更新cc大佬通知
-[更多]没了
+2022.8.9 (v1.4.0)
+    * 修复报错问题(Empty or invalid UTF-8 xxxx)
+[更多](https://github.com/curtinlv/gd/blob/main/updateLog.md)
 """
