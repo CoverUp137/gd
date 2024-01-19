e4630bc55e179019bbede3e650d557352af9869a
coverUP2 <73152816+coverUP2@users.noreply.github.com>
2024-01-19 23:16:11 +0800

    Add files via upload

--- diy/jxjd_Global.py
+++ diy/jxjd_Global.py
@@ -1,47 +1,49 @@
 from telethon import events
-from .. import user
-from ..diy.utils import read
-import requests
-import os
+from .. import jdbot
+from ..diy.utils import read, write
+import re
+import requests,json
 
+try:
+    from .login import user
+except:
+    from .. import user
+    
 @user.on(events.NewMessage(pattern=r'^jx', outgoing=True))
 async def jcmd(event):
-    strText = ""
-    if event.is_reply:
+    strText=""
+    if event.is_reply is True:
         reply = await event.get_reply_message()
-        strText = reply.text
-    else:
-        msg_text = event.raw_text.split(' ')
+        strText=reply.text
+    else:    
+        msg_text= event.raw_text.split(' ')
         if isinstance(msg_text, list) and len(msg_text) == 2:
             strText = msg_text[-1]
-
-    if not strText:
-        await user.send_message(event.chat_id, '请指定要解析的口令，格式：jx 口令 或对口令直接回复jx')
-        return
     
-    # 先检查青龙环境变量是否有jdjx_sign
-    jdjx_sign = os.environ.get('jdjx_sign')
-    if not jdjx_sign:
-        # 如果没有，再检查是否在 /ql/config/config.sh 文件中
-        configs = read("str")
-        for config in configs.split('\n'):
-            if 'jdjx_sign' in config:
-                jdjx_sign = config.split('=')[1].strip('"')
-                break
-    
-    if not jdjx_sign:
-        await user.send_message(event.chat_id, '请先自建sign然后设置 jdjx_sign 变量')
-        return
-
-    data = {"key": strText}
-    response = requests.post(jdjx_sign, data=data, headers={"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8;"})
-    if response.status_code == 200:
-        result = response.json()
-        if result["code"] == 200:
-            title = result["data"]["title"]
-            jump_url = result["data"]["jumpUrl"]
-            await user.send_message(event.chat_id, f"{title}\n{jump_url}")
-        else:
-            await user.send_message(event.chat_id, f"解析出错：{result['data']}")
+    if strText==None:
+        await user.send_message(event.chat_id,'请指定要解析的口令,格式: jx 口令 或对口令直接回复jx ')
+        return 
+        
+    jumpUrl=""
+    title=""    
+    jiexiurl = "http://api.nolanstore.cc/JComExchange"    
+    data ={"code": strText}
+    headers={"Content-Type": "application/json"}
+    issuccess=False
+    for num in range(10):  
+        try:
+            res=requests.post(url=jiexiurl,headers=headers,json=data,timeout=3)
+            issuccess=True
+        except:
+            issuccess=False
+        if issuccess:
+            break
+    resdata=json.loads(res.text)
+    if resdata["code"]=="0":  
+        title = resdata["data"]['title']
+        jumpUrl = resdata["data"]['jumpUrl']
+        
+    if jumpUrl != "":        
+        await user.send_message(event.chat_id,title+"\n"+jumpUrl)
     else:
-        await user.send_message(event.chat_id, "请求出错，HTTP状态码：" + str(response.status_code))
+        await user.send_message(event.chat_id,"解析出错:\n"+res.text)
